662fb4086d1ef4460f1fa66cc9f155491ae788b5
Angelina <linaemalmros@gmail.com>
2023-01-09 09:08:43 +0100

    continued working on joinGame

--- src/main/java/com/example/rps/game/GameService.java
+++ src/main/java/com/example/rps/game/GameService.java
@@ -1,5 +1,6 @@
 package com.example.rps.game;
 
+import com.example.rps.NotFoundException;
 import com.example.rps.player.PlayerEntity;
 import com.example.rps.player.PlayerRepository;
 import lombok.AllArgsConstructor;
@@ -42,18 +43,26 @@ public class GameService {
         return gameStatus;
     }
 
-    public GameStatus joinGame(UUID gameId, UUID playerId) {
+    public GameStatus joinGame(UUID gameId, UUID playerId) throws NotFoundException {
         PlayerEntity playerEntity = new PlayerEntity();     //this doesn't work, but I have had a bottle of wine, I coded anyway, but am calling it for today
 
         // här vill vi hitta ett spel via gameId, visa spelaren som redan är ansluten,
         // lägga in spelare 2 (namn & id), samt ändra status på spelet till ACTIVE
 
-        gameRepository.getReferenceById(gameId);  // här hämtar vi spelet som startades i metoden ovan
+        Optional<GameEntity> gameEntity = gameRepository.findById(gameId);  // här hämtar vi spelet som startades i metoden ovan
 
+        if (gameEntity.isPresent()) {
+            gameEntity.get().setGameStatus(ACTIVE);
+            gameEntity.get().setPlayerTwo(playerEntity.getPlayerTwoGame().getPlayerTwo());
+            gameEntity.get().setPlayerTwo(playerRepository.findById(playerId).get());
+        }
+        else {
+            throw new NotFoundException("Game not found");
+        }
         // if gameId exists .. bla bla
         // else  bla bla
 
-        GameStatus gameStatus = new GameStatus(
+        return new GameStatus(
                 gameId,
                 playerEntity.getPlayerOneGame().getPlayerOne(),
                 null,
@@ -61,15 +70,6 @@ public class GameService {
                 null,
                 ACTIVE
         );
-
-        GameEntity gameEntity = new GameEntity();
-        gameEntity.setGameStatus(ACTIVE);
-        gameEntity.setPlayerTwo(playerEntity.getPlayerTwoGame().getPlayerTwo());
-        gameEntity.setPlayerTwo(playerRepository.findById(playerId).get());
-
-        gameRepository.save(gameEntity);
-
-        return gameStatus;
     }
 /*
     public Optional<GameEntity> joinGame(UUID gameId, GameStatus gameStatus) {
