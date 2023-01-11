50f34c169b29da091611f5c49a7c6755a5636abe
Angelina <linaemalmros@gmail.com>
2023-01-11 15:12:22 +0100

    wip applicationconfig

--- src/main/java/com/example/rps/security/config/ApplicationConfig.java
+++ src/main/java/com/example/rps/security/config/ApplicationConfig.java
@@ -4,9 +4,13 @@ import com.example.rps.player.PlayerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -18,5 +22,19 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService() {
         return username -> (UserDetails) playerRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {  // DAO that fetches user details, encodes password etc
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
