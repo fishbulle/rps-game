f4da251ce91ac508293f170174d9a7d68391a3f5
Angelina <linaemalmros@gmail.com>
2023-01-11 13:16:37 +0100

    added comment for future ideas

--- src/main/java/com/example/rps/game/Result.java
+++ src/main/java/com/example/rps/game/Result.java
@@ -2,7 +2,9 @@ package com.example.rps.game;
 
 public enum Result {
 
-    // to use later instead of putting these in the Status class
+    // to use later instead of using the Status enum
+    // so after x rounds are finished, game status will be none/finished
+    // and the result will be showed in a different column in database
 
     WIN,
     DRAW,
